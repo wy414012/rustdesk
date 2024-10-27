c833310124a5f0eb64e25cff1c69508d3e7d2af3
Follow the wind <36281137+wy414012@users.noreply.github.com>
2024-10-27 17:24:07 +0800

    将默认服务器地址和key修改为占位地址，保证为空不默认连接到任何服务器

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.free.rustdesk.v6.pub"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["free0.rustdesk.v6.pub"];
+pub const PUBLIC_RS_PUB_KEY: &str = "OeVuasfdsafadsfsafdasgsagsaBw=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
