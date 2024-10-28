41e3430c78db555847fcad060fb556e9e5b3e8af
ymwl <ymwlpoolc@qq.com>
2024-10-28 16:19:28 +0800

    调整默认key

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["free0.rustdesk.v6.pub"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuasfdsafadsfsafdasgsagsaBw=";
+pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
